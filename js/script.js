630e24ecaa0568a8f853ba70c4a42bc58518cf45
Denis-Turbatu <turbatudenis81@gmail.com>
2024-04-19 17:09:33 +0200

    feat: splice for list items

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ createApp({
                 mark: "",
                 object: [],
                 tag: "",
+                done: "",
             },
             newTodo: {
                 mark: "",
@@ -20,10 +21,22 @@ createApp({
     methods: {
         addTodo: function(){
             this.todo.object.push(this.newTodo.object);
-            
+            this.todo.mark = `X`;
+            this.todo.done = false;
+
             this.newTodo.mark = "";
             this.newTodo.object = "";
             this.newTodo.tag = "";
+        },
+        doneItem: function(curItem, index) {
+            this.todo.mark = true;
+            this.todo.done = true;
+            if (this.todo.mark == true && this.todo.done == true) {
+                this.todo.mark = `V`;
+            }
+        },
+        removeList: function (curItem, index) {
+            this.todo.object.splice(index, 1)
         }
     }
 }).mount('#app');
